708c2ab6427e0ec1e2e74bb6192c3e901fe55951
Matthew McCourry <m.mccourry@gmail.com>
2023-07-20 17:22:50 -0700

    playing with lists

---
+++ Automate/lists.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+###############
+# Author : Matthew McCourry
+# Contact: github.com/mbmcc
+# Title : Lists
+# Description : <https://automatetheboringstuff.com/2e/chapter3/>
+# 
+# Created : Wed Jul 19 2023
+# Last modified : same
+###############
